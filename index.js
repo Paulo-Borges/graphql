4c9c787bad94b1d09d7f36635e5a3bfb68464c9e
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-11-12 21:12:26 -0300

    feat: Adiciona .gitignore e para de rastrear arquivos ignorados

---
+++ index.js
@@ -0,0 +1,50 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql`
+  scalar Data
+
+  type Query {
+    ola: String
+    horaAtual: String
+    dataAtual: Data
+    melhorUsuario: Usuario!
+  }
+
+  type Usuario {
+    id: Int
+    nome: String
+    email: String
+    salario: Float
+    vip: Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    ola() {
+      return "Olá Mundo!";
+    },
+    horaAtual() {
+      const dataAtual = new Date();
+      return ` ${dataAtual.getHours()}: ${dataAtual.getMinutes()}`;
+    },
+    dataAtual() {
+      return new Date();
+    },
+    melhorUsuario() {
+      return {
+        id: 1,
+        nome: "Borges",
+        email: "borges@email.com",
+        salario: 16000.0,
+        vip: true,
+      };
+    },
+  },
+};
+
+const servidor = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(servidor);
+console.log(`Servidor rodando em ${url}`);
